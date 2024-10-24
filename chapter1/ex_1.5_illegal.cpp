8416e9768e88f58baf71c968f5173a9637760983
Asia Besant <75949692+ihoneybee@users.noreply.github.com>
2024-10-24 09:26:16 -0400

    Update ex_1.5_illegal.cpp

--- chapter1/ex_1.5_illegal.cpp
+++ chapter1/ex_1.5_illegal.cpp
@@ -4,9 +4,9 @@ int main(){
     std::cout << "Enter two numbers: " << std::endl;
     int v1 = 0, v2 = 0;
     std::cin >> v1 >> v2;
-    std::cout << "The Product of " << v1;
-    << "and " << v2;
-    << " is " << v1 * v2 << std::endl;
+    std::cout << "The Product of " << v1; 
+    << "and " << v2; // illegal
+    << " is " << v1 * v2 << std::endl; // illegal
 
     return 0;
 }
