9c663fe46cf7f296e63d9e06f2431d108454aabb
Asia Besant <75949692+ihoneybee@users.noreply.github.com>
2024-10-25 12:08:26 -0400

    Update ex_1.9.cpp

--- chapter1/ex_1.9.cpp
+++ chapter1/ex_1.9.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
 
+int main(){
+
+    int sum = 0, val = 50;
+    while( val <= 100){
+        sum += val;
+        ++ val;
+    }
+    std::cout << "The sum of values from 50 to 100 is " << sum << std::endl;
+
+    return 0;
+}
